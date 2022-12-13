e85606b3af3925aa1f3eb8200e56e722bf081dc4
Jacob L. Walker <jacob.walker@ramseysolutions.com>
2022-12-13 11:41:41 -0600

    apply layout to app content and update title for clarity

--- src/App/__tests__/App.test.tsx
+++ src/App/__tests__/App.test.tsx
@@ -1,13 +1,38 @@
 import App from '../App';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('App Integration Tests', () => {
   it('renders the header of the app', () => {
     render(<App />);
 
-    const headerElement = screen.getByText(/To Do App/i);
+    const headerElement = screen.getByText(/To-Do List App/i);
 
     expect(headerElement).toBeInTheDocument();
   });
+
+  it('renders the to-do list', () => {
+    render(<App />);
+
+    const toDoListElement = screen.getByTestId('to-do-list');
+
+    expect(toDoListElement).toBeInTheDocument();
+  });
+
+  it('opens the Add New Task form when the add new task button is clicked', async () => {
+    const user = userEvent.setup();
+    render(<App/>);
+
+    const addNewTaskButton = await screen.findByRole('button', { name: '+ New Task' });
+    await user.click(addNewTaskButton);
+
+    const addNewTaskForm = await screen.findByText(/Add New Task/i);
+
+    expect(addNewTaskForm).toBeInTheDocument();
+  });
+
+  // it('renders the detail card when a to-do list item is selected', () => {
+  //
+  // });
 });
--- src/ToDoHeader/ToDoHeader.tsx
+++ src/ToDoHeader/ToDoHeader.tsx
@@ -5,7 +5,7 @@ import typescriptLogo from '../assets/ts-logo-128.svg';
 const ToDoHeader = () => {
   return (
     <header className="ToDoHeader">
-      <h1 className="ToDoHeader-title">To Do App</h1>
+      <h1 className="ToDoHeader-title">To-Do List App</h1>
       <h4 className="ToDoHeader-subtitle">
         Created with <img src="./vite.svg" aria-hidden={true} alt="Vite logo" /> Vite +{' '}
         <img src={reactLogo} aria-hidden={true} alt="React logo" /> React +{' '}
