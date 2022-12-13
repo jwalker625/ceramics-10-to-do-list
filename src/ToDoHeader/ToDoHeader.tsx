7d85ce75df238938033a3a1eed58df29e06bb483
Jacob L. Walker <jacob.walker@ramseysolutions.com>
2022-12-13 10:48:02 -0600

    style header

--- src/ToDoHeader/ToDoHeader.tsx
+++ src/ToDoHeader/ToDoHeader.tsx
@@ -5,12 +5,12 @@ import typescriptLogo from '../assets/ts-logo-128.svg';
 const ToDoHeader = () => {
   return (
     <header className="ToDoHeader">
-      <h1>To Do App</h1>
-      <h3>
+      <h1 className="ToDoHeader-title">To Do App</h1>
+      <h4 className="ToDoHeader-subtitle">
         Created with <img src="../../public/vite.svg" /> Vite +{' '}
         <img src={reactLogo} /> React +{' '}
         <img src={typescriptLogo} /> TypeScript
-      </h3>
+      </h4>
     </header>
   );
 }
